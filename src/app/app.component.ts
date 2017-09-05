b00b1a9b4ed652dd485a31214b6a2ccdb089d60c
rohitbhatalia <31652320+rohitbhatalia@users.noreply.github.com>
2017-09-05 15:43:09 +0530

    my first commit to it

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>{{title}}</h1>
+  <nav>
+    <a routerLink="/dashboard">Dashboard</a>
+    <a routerLink="/heroes">Heroes</a>
+  </nav>
+  
+`
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
