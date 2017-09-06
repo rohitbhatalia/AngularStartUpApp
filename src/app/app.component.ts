095bfb3e661941565ed93fba97b66e04fcf8aea7
rohitbhatalia <31652320+rohitbhatalia@users.noreply.github.com>
2017-09-06 13:57:22 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
     <a routerLink="/dashboard">Dashboard</a>
     <a routerLink="/heroes">Heroes</a>
   </nav>
-  
+  <router-outlet></router-outlet>
 `
 })
 export class AppComponent {
